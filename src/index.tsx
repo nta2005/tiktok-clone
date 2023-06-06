ff53ba1118f1eb18b504e2ddf27ea692dff0e690
an.nguyen2 <an.nguyen2@bnksolution.com>
2023-06-06 14:32:04 +0700

    add lib babel-plugin-module-resolver

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import * as Learn from './learn';
+import 'index.css';
+import App from 'App';
+import reportWebVitals from 'reportWebVitals';
+import * as Learn from 'learn';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
