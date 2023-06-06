0589f2b4c9f55a04727c4cb8a1ff964213e5f9f8
an.nguyen2 <an.nguyen2@bnksolution.com>
2023-06-06 17:14:09 +0700

    config router/layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,44 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from 'routes';
+import { DefaultLayout } from 'components/Layout';
+
 function App() {
 	return (
-		<div>
-			<h1>React App</h1>
-		</div>
+		<Router>
+			<Routes>
+				{publicRoutes.map((route: any, index: number) => {
+					const Page = route.component;
+
+					// let Layout: any = DefaultLayout;
+
+					// if (route.layout) {
+					// 	Layout = route.layout;
+					// } else if (route.layout === null) {
+					// 	Layout = React.Fragment;
+					// }
+
+					let Layout = route.layout
+						? route.layout
+						: route.layout === null
+						? React.Fragment
+						: DefaultLayout;
+
+					return (
+						<Route
+							key={index}
+							path={route.path}
+							element={
+								<Layout>
+									<Page />
+								</Layout>
+							}
+						/>
+					);
+				})}
+			</Routes>
+		</Router>
 	);
 }
 
---
+++ src/routes/index.ts
@@ -0,0 +1,16 @@
+import { Home, Following, Profile, Upload, Search } from 'pages';
+import { HeaderOnly } from 'components/Layout';
+
+//Public routes
+const publicRoutes = [
+	{ path: '/', component: Home },
+	{ path: '/following', component: Following },
+	{ path: '/search', component: Search, layout: null },
+];
+
+const privateRoutes = [
+	{ path: '/profile', component: Profile },
+	{ path: '/upload', component: Upload, layout: HeaderOnly },
+];
+
+export { publicRoutes, privateRoutes };
